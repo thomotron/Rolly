e5701f8f781afa587f8a8d597922291c9b2352e2
Thomas Wade <tom@tem.party>
2023-04-02 19:46:23 +0930

    Add NamesCaseSensitive setting
    This allows name searches to be treated as either case-sensitive or
    case-insensitive.
    
    Fixes #10

--- rolly.go
+++ rolly.go
@@ -28,12 +28,13 @@ type Config struct {
 	}
 
 	Discord struct {
-		ApplicationID     string
-		BotToken          string
-		BotOwners         []string
-		BotServer         string
-		RollCallChannelID string
-		ReactionColours   map[string]ColourPriority
+		ApplicationID      string
+		BotToken           string
+		BotOwners          []string
+		BotServer          string
+		RollCallChannelID  string
+		ReactionColours    map[string]ColourPriority
+		NamesCaseSensitive bool
 	}
 }
 
@@ -639,17 +640,28 @@ func findNameCell(sheets *sheets.Service, sheetID string, name string, ranges ..
 
 		for majorIndex, majorDimension := range valueRange.Values {
 			for minorIndex, cell := range majorDimension {
-				if cellValue, isString := cell.(string); isString && cellValue != "" && strings.Contains(strings.TrimSpace(name), strings.TrimSpace(cellValue)) {
-					// Found a match
-					// Now figure out whether we were iterating horizontally or vertically to apply the appropriate
-					// offsets
-					if valueRange.MajorDimension == "COLUMNS" {
-						// Outer iterator was per column
-						return xOffset + majorIndex, yOffset + minorIndex, nil
-					} else /*if valueRange.MajorDimension == "ROWS"*/ { // Always default to rows as this is standard
-						// Outer iterator was per row
-						return xOffset + minorIndex, yOffset + majorIndex, nil
+				if cellValue, isString := cell.(string); isString && cellValue != "" {
+					// Clean up the values a bit
+					cellValue = strings.TrimSpace(cellValue)
+					name = strings.TrimSpace(name)
+					if !config.Discord.NamesCaseSensitive {
+						cellValue = strings.ToLower(cellValue)
+						name = strings.ToLower(name)
 					}
+
+					if strings.Contains(name, cellValue) {
+						// Found a match
+						// Now figure out whether we were iterating horizontally or vertically to apply the appropriate
+						// offsets
+						if valueRange.MajorDimension == "COLUMNS" {
+							// Outer iterator was per column
+							return xOffset + majorIndex, yOffset + minorIndex, nil
+						} else /*if valueRange.MajorDimension == "ROWS"*/ { // Always default to rows as this is standard
+							// Outer iterator was per row
+							return xOffset + minorIndex, yOffset + majorIndex, nil
+						}
+					}
+
 				}
 			}
 		}
